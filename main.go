d4098e4312665666254c79b746a94e9f925a9f56
Justin <juc81@pitt.edu>
2023-11-25 14:54:45 -0500

    isolate process in own env - changing root fs so processes cant see host system processes/fs

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"syscall"
+	"syscall" // https://pkg.go.dev/syscall#pkg-constants
 )
 
 func main() {
@@ -21,9 +21,9 @@ func main() {
 // create "Command" too run the same executable -> proc self exe is the currently runing program 
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...) // append rest of cli arguments after "child"
-	// add  UTS, PID, MNT namespaces
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	// added  UTS, PID, MNT namespaces ... add IPC/user/net later
+	cmd.SysProcAttr = &syscall.SysProcAttr{ // system attributes for new process
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS, 
 	}
 
 	// set input, output, and error streams to the streams of parent process
@@ -38,6 +38,12 @@ func parent() {
 }
 // parent calls child, creating a command based on subsequent arguments
 func child() {
+	// swap to root fs by performing a bind mount - attach rootfs directory to itself, making it available in more than one location
+	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+		must(os.MkdirAll("rootfs/oldrootfs", 0700)) // new directory "oldrootfs" w/ permission to read/write/execute only for owner 
+		// move current directory at "/" to "rootfs/oldrootfs". Pivotroot will swap "rootfs" to "/" -> OS requires for bind mount call that swapping FS are not part of same tree , we can mount to self
+		must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+		must(od.Chdir("/")) // change pwd to new root fs
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
